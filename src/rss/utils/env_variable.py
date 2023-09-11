d871ef9e1e9528d94c10dd8848593cb24b19d4c4
Daniel Ibarrola <dis96051@gmail.com>
2023-09-11 16:28:21 -0600

    Updated cap format and removed old formats

--- src/rss/utils/env_variable.py
+++ src/rss/utils/env_variable.py
@@ -9,6 +9,7 @@ class MissingEnvVariableError(ValueError):
 def get_env_variable(name: str, var_type: Type[str] | Type[int] = str) -> str | int:
     """ Check that an environment variable is set in production mode.
     """
+    # TODO: unit test this function
     config = os.environ.get("CONFIG", "dev")
     if "prod" in config:
         value = os.environ.get(name, None)
@@ -19,6 +20,7 @@ def get_env_variable(name: str, var_type: Type[str] | Type[int] = str) -> str | 
         if var_type is str:
             return value
         elif var_type is int:
+            # TODO: fix
             return int(var_type)
         raise ValueError(f"Invalid var type {var_type}")
 
