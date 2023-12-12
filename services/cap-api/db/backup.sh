b7fb0354abbb348fddcec49683c1c515d73f392f
Daniel Ibarrola <dis96051@gmail.com>
2023-12-12 10:41:43 -0600

    Bumped socketlib to 0.6.1 in cap generator requirements

--- services/cap-api/db/backup.sh
+++ services/cap-api/db/backup.sh
@@ -11,12 +11,12 @@ TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 BACKUP_FILENAME="backup_$TIMESTAMP.dump"
 BACKUP_FILE="$BACKUP_DIR/$BACKUP_FILENAME"
 
-docker compose exec postgres pg_dump -U "$DB_USER" -F c -b -v -f "$BACKUP_FILE" "$DB_NAME"
+docker exec rss-db pg_dump -U "$DB_USER" -F c -b -v -f "$BACKUP_FILE" "$DB_NAME"
 
 if [ $? -eq 0 ]; then
     echo "Database backup completed successfully at $(date +'%Y-%m-%d %H:%M:%S')."
 
-    BACKUP_FILE="../../../$BACKUP_DIR/$BACKUP_FILENAME"
+    BACKUP_FILE="/home/daniel/RSS/pg_backup/$BACKUP_FILENAME"
     S3_BUCKET="cap-rss-pg-backups"
     /usr/local/bin/aws s3 cp "$BACKUP_FILE" "s3://$S3_BUCKET"
 
@@ -25,8 +25,6 @@ if [ $? -eq 0 ]; then
     else
         echo "Failed to upload backup to S3."
     fi
-
-    rm "$BACKUP_FILE"
 
 else
     echo "Database backup failed at $(date +'%Y-%m-%d %H:%M:%S')."
